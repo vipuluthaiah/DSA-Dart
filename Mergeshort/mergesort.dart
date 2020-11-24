35908893665017a73c569f9e1077c358f0f6fb15
vipuluthaiah <kechamadavipul@gmail.com>
2020-11-24 22:01:31 +0530

    quick sort

--- Mergeshort/mergesort.dart
+++ Mergeshort/mergesort.dart
@@ -1,30 +1,81 @@
-//merge sort in dart
+// //merge sort in dart
 
-import 'dart:math' as Math;
+// import 'dart:math' as Math;
 
+// merge(List array1, List array2) {
+//   var i = 0;
+//   var j = 0;
+//   var results = [];
+
+//   while (i < array1.length && j < array2.length) {
+//     if (array2[j] >= array1[i]) {
+//       results.add(array1[i]);
+//       i++;
+//     } else {
+//       results.add(array2[j]);
+//       j++;
+//     }
+//   }
+//   while (i < array1.length) {
+//     results.add(array1[i]);
+//     i++;
+//   }
+//   while (j < array2.length) {
+//     results.add(array2[j]);
+//     j++;
+//   }
+//   return results;
+// }
+
+// mergeSort(List array) {
+//   if (array.length <= 1) return array;
+
+//   var mid = (array.length) ~/ 2;
+//   var left = mergeSort(array.sublist(0, mid));
+//   var right = mergeSort(array.sublist(mid));
+//   return merge(right, left);
+// }
+
+// main() {
+//   final seed = 10000000;
+//   var rnd = Math.Random();
+
+//   var list = List.generate(seed, (i) => rnd.nextInt(seed), growable: false);
+//   // list.sort();
+//   // print(list);
+
+//   // print("Before Sorting");
+//   // print("");
+//   // print(list);
+//   // print("");
+
+//   // // print(merge([1, 4, 6], [2, 3, 7]));
+//   print("After Sorting");
+//   print(mergeSort(list));
+// }
 merge(List array1, List array2) {
   var i = 0;
   var j = 0;
-  var results = [];
+  var result = [];
 
   while (i < array1.length && j < array2.length) {
     if (array2[j] >= array1[i]) {
-      results.add(array1[i]);
+      result.add(array1[i]);
       i++;
     } else {
-      results.add(array2[j]);
+      result.add(array2[j]);
       j++;
     }
   }
   while (i < array1.length) {
-    results.add(array1[i]);
+    result.add(array1[i]);
     i++;
   }
   while (j < array2.length) {
-    results.add(array2[j]);
+    result.add(array2[j]);
     j++;
   }
-  return results;
+  return result;
 }
 
 mergeSort(List array) {
@@ -33,23 +84,12 @@ mergeSort(List array) {
   var mid = (array.length) ~/ 2;
   var left = mergeSort(array.sublist(0, mid));
   var right = mergeSort(array.sublist(mid));
-  return merge(right, left);
+  return merge(left, right);
 }
 
 main() {
-  final seed = 10000000;
-  var rnd = Math.Random();
+  var array = [33, 22, 1, 2, 3, 77, 221];
 
-  var list = List.generate(seed, (i) => rnd.nextInt(seed), growable: false);
-  // list.sort();
-  // print(list);
 
-  // print("Before Sorting");
-  // print("");
-  // print(list);
-  // print("");
-
-  // // print(merge([1, 4, 6], [2, 3, 7]));
-  print("After Sorting");
-  print(mergeSort(list));
+  print(mergeSort(array));
 }
--- quicksort/quicksort.dart
+++ quicksort/quicksort.dart
@@ -1,53 +1,35 @@
-//quick sort
+import 'dart:math' show Random;
 
-main() {
-  // print(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
-  var list = [];
-  // print(quickSort([23,4,5,65,6,7,7], left, right) )
-  // print(quickSort(list, this.left, right));
-  var c = quicksort();
-  print(c.quickSort(list, c.left, list.length - 1));
+// quickSort
+// O(n*log n)
+void main() {
+  var list = List<int>();
+  Random random = new Random();
+  for (var i = 0; i < 100; i++) {
+    list.add(random.nextInt(100));
+  }
+  print('before sorting:');
+  print(list);
+  print('---------------------------------------------');
+  print('After sorting:');
+  print(quickSort(list));
 }
 
-pivot(List<int> array, int start, int end) {
-  swap(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-
-  start = 0;
-  end = array.length + 1;
-
-  var pivot = array[start];
-  var swapindex = start;
-
-  for (var i = start + 1; i < array.length; i++) {
-    if (pivot > array[i]) {
-      swapindex++;
-      swap(array, swapindex, i);
+List<int> quickSort(List<int> a) {
+  if (a.length < 2) {
+    return a;
+  } else {
+    var pivot = a[0];
+    var less = <int>[];
+    var greater = <int>[];
+    a.removeAt(0);
+    for (var i in a) {
+      if (i <= pivot) {
+        less.add(i);
+      } else {
+        greater.add(i);
+      }
     }
+    return quickSort(less) + [pivot] + quickSort(greater);
   }
-  swap(array, start, swapindex);
-  return swapindex;
-  // print(swapindex);
-}
-
-class quicksort {
-  quickSort(List arr, int left, int right) {
-    if (arr.isEmpty) {
-      return 0;
-    }
-    if (left < right) {
-      var pivoteIndex = pivot(arr, left, right);
-      //left
-      quickSort(arr, left, pivoteIndex - 1);
-      //right
-      quickSort(arr, pivoteIndex + 1, right);
-    }
-
-    return arr;
-  }
-
-  var left = 0;
 }
